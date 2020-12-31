e281c884af94084b8c99322c78b7cfd46b5d157b
hirenbhal <iamhiren5@gmail.com>
2020-12-31 13:56:52 +0530

    initial commit

---
+++ client/build/precache-manifest.5d6903c6ebdfff76f74bca2deb61a777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168b5eb450000ee79f1ea6e93d23076d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a52f535ccc49ed7f7f6b",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "37d469f760c8b0756102",
+    "url": "/static/js/2.164e6664.chunk.js"
+  },
+  {
+    "revision": "a52f535ccc49ed7f7f6b",
+    "url": "/static/js/main.0335809f.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
